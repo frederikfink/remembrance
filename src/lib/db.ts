bceefea99f7714afe84768c3c955301e7b7278b0
Frederik Fink Højriis <frfh@itu.dk>
2023-01-06 16:34:23 +0100

    update db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,7 @@
-import { env } from '$env/dynamic/public';
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 
-const supabaseUrl = env.PUBLIC_SUPABASE_URL as string
-const supabaseAnonKey = env.PUBLIC_SUPABASE_ANON_KEY as string
+const supabaseUrl = process.env.PUBLIC_SUPABASE_URL as string
+const supabaseAnonKey = process.env.PUBLIC_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(
     supabaseUrl,
