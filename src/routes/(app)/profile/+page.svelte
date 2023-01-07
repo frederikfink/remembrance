e46969e53ff372458bc9962c25da80b32ea1bb3a
Frederik Fink Højriis <frfh@itu.dk>
2023-01-07 11:55:31 +0100

    add modals to group select and add

--- src/routes/(app)/profile/+page.svelte
+++ src/routes/(app)/profile/+page.svelte
@@ -3,6 +3,9 @@
 	import { supabase } from '$lib/db';
 	import Group from './Group.svelte';
 
+	let addGroupTitle:string = '';
+	let addGroupDescription:string = '';
+
 	const signOut = async () => {
 		const { error } = await supabase.auth.signOut();
 
@@ -12,28 +15,62 @@
 	const fetchGroups = async () => {
 
 	}
+
+	const addGroup = async () => {
+		console.log('test!');
+	}
+	
+	const updateGroup = async () => {
+		
+	}
 </script>
 
 <div>
 	<h2 class="py-8 px-2 border-b border-base-300 text-2xl font-bold">Groups</h2>
 	<div class="py-4 flex flex-col gap-2 border-b border-base-300">
 
-		<Group title={'Title'} members={5} items={400} />
-		<Group title={'really long title that should break ui somehow but it needs to be really long '} members={0} items={20} />
-		<Group title={'my'} members={5} items={2} />
-		<Group title={'my title'} members={5} items={400} />
+		<Group title={'Title'} members={5} items={400} slug={"asd"}/>
+		<Group title={'really long title that should break ui somehow but it needs to be really long '} members={0} items={20} slug={"asdasd"}/>
+		<Group title={'my'} members={5} items={2} slug={"asdasdasd"}/>
+		<Group title={'my title'} members={5} items={400} slug={"asdasdasdasdasd"}/>
 
 	</div>
 
 	<div class="flex py-4 px-2">
-		<button class="btn gap-2 ml-auto">
+		<label for="create-group-modal" class="btn gap-2 ml-auto">
 			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
 				<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 			</svg>
 			Create group
-		</button>
+		</label>
+	
+		<!-- Put this part before </body> tag -->
+		<input type="checkbox" id="create-group-modal" class="modal-toggle" />
+			<label for="create-group-modal" class="modal cursor-pointer modal-bottom sm:modal-middle">
+			<label class="modal-box relative" for="">
+				<h3 class="font-semibold text-xl">Create new group</h3>
+				<form id="create-group-form" on:submit|preventDefault={addGroup}>
+					<div class="w-full">
+						<label class="label" for="group-name-input">
+							  <span class="label-text">Group Title</span>
+						</label>
+						<input bind:value={addGroupTitle} id="group-name-input" type="text" placeholder="Type here" class="input input-bordered w-full" />
+					</div>
+					<div class="w-full">
+						<label class="label" for="group-name-input">
+							  <span class="label-text">Group Description</span>
+						</label>
+						<input bind:value={addGroupDescription} id="group-description-input" type="text" placeholder="Type here" class="input input-bordered w-full" />
+					</div>
+				</form>
+				<div class="modal-action">
+					<input form="create-group-form" type="submit" class="btn btn-success" value="Create!" />
+				</div>
+			</label>
+		</label>	
 	</div>
-		
+
+
 </div>
 
 <div class="mx-2 mt-24">
@@ -41,3 +78,4 @@
 		<button type="submit" class="btn btn-outline btn-error gap-2"> Log out </button>
 	</form>
 </div>
+
--- src/routes/(app)/profile/Group.svelte
+++ src/routes/(app)/profile/Group.svelte
@@ -3,26 +3,94 @@
 export let title:string;
 export let items:number;
 export let members:number;
+export let slug:string;
+export let description:string;
+
+let newTitle: string = title;
+let newDescription: string = description;
+let inviteLink: string = '/lol';
+let showToast:boolean = false;
+
+const updateGroup = async () => {
+    
+}
+
+const copyInviteLink = () => {
+    navigator.clipboard.writeText(inviteLink);
+    toggleToast();
+}
+
+
+const toggleToast = async () => {
+	showToast = true;
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    showToast = false;
+}
 
 </script>
 
 <div class="grid grid-cols-6 items-center gap-2 px-2 text-left ">
 
+    {#if showToast}
+    <div class="toast toast-top toast-start">
+        <div class="alert alert-success">
+            <div>
+            <span>Copied invite link to clipboard</span>
+            </div>
+        </div>
+    </div>
+    {/if}
+    
     <div class="col-span-4">
         <p class="font-medium tracking-tight truncate">{title}</p>
         <p class="text-sm">{items > 99 ? '99+' : items} items | {members} members</p>
     </div>
 
-    <button class="btn btn-accent">
+    <label for="modal-{slug}" class="btn btn-accent">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-          </svg>				  
-    </button>
+          </svg>		
+    </label>
 
-    <a href="/" class="btn btn-primary">
+    <a href="/{slug}" class="btn btn-primary">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
         </svg>
     </a>
+
+    <!-- Put this part before </body> tag -->
+    <input type="checkbox" id="modal-{slug}" class="modal-toggle" />
+        <label for="modal-{slug}" class="modal cursor-pointer modal-bottom sm:modal-middle">
+        <label class="modal-box relative" for="">
+            <h3 class="font-semibold text-xl truncate">Update {title}</h3>
+
+            <form id="create-group-form" on:submit|preventDefault={updateGroup}>
+                <div class="w-full">
+                    <label class="label" for="group-name-input">
+                          <span class="label-text">Group Title</span>
+                    </label>
+                    <input bind:value={newTitle} id="group-name-input" type="text" placeholder="Type here" class="input input-bordered w-full" />
+                </div>
+                <div class="w-full">
+                    <label class="label" for="group-name-input">
+                          <span class="label-text">Group Description</span>
+                    </label>
+                    <input bind:value={newDescription} id="group-description-input" type="text" placeholder="Type here" class="input input-bordered w-full" />
+                </div>
+            </form>
+
+            <div class="w-full my-2">
+                <p class="label-text">Invite more members, send invite link</p>
+                <button on:click={copyInviteLink} class="btn w-full mt-2">
+                    Invite!
+                </button>
+            </div>
+
+            <div class="modal-action">
+                <input form="create-group-form" type="submit" class="btn btn-success" value="Update!" />
+            </div>
+
+        </label>
+    </label>	
 </div>
